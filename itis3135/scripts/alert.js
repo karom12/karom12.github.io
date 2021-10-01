effd405d796081f73983d37f7e30ae350b567adf
karom12 <kromano@uncc.edu>
2021-10-01 17:23:33 -0400

    Update alert.js

--- itis3135/scripts/alert.js
+++ itis3135/scripts/alert.js
@@ -7,5 +7,5 @@ function askName()
     var name = prompt("What is your name?");
     var feeling = prompt("How are you doing?");
     const d1 = new Date();
-    alert("Today is "+ d1.toLocaleDateString +",and the time is: The Totally-Not-Doing-Illegal-Things LLC welcomes you, " + name +"! We're glad you are doing " + feeling +"!")
+    alert("Today is "+ d1.getMonth()+"," + d1.getDate()+"," + d1.getFullYear + ", and the time is: The Totally-Not-Doing-Illegal-Things LLC welcomes you, " + name +"! We're glad you are doing " + feeling +"!")
 }
