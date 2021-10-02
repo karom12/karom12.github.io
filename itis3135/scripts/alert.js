62f969157ada791fbcc0b0e9c156f9489518b2e0
karom12 <kromano@uncc.edu>
2021-10-02 00:03:40 -0400

    Update alert.js

--- itis3135/scripts/alert.js
+++ itis3135/scripts/alert.js
@@ -1,3 +1,7 @@
+var name = prompt("What is your name?");
+    var feeling = prompt("How are you doing?");
+    const d1 = new Date();
+    alert("Today is "+ d1 + ". The Totally-Not-Doing-Illegal-Things LLC welcomes you, " + name +"! We're glad you are doing " + feeling +"!");
 function test1()
 {
     alert("Hey my script is running");
