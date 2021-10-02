3c1dcd6c6c6d6bcd6530ba2fdd3674d44f936fa8
karom12 <kromano@uncc.edu>
2021-10-01 22:27:37 -0400

    1

--- itis3135/scripts/alert.js
+++ itis3135/scripts/alert.js
@@ -7,5 +7,23 @@ function askName()
     var name = prompt("What is your name?");
     var feeling = prompt("How are you doing?");
     const d1 = new Date();
-    alert("Today is "+ d1 + ". The Totally-Not-Doing-Illegal-Things LLC welcomes you," + name +"! We're glad you are doing " + feeling +"!")
+    alert("Today is "+ d1 + ". The Totally-Not-Doing-Illegal-Things LLC welcomes you, " + name +"! We're glad you are doing " + feeling +"!");
+}
+function weSell(){
+    alert("We totally don't sell anything illegal");
+
+}
+function weAreReal(){
+    alert("We totally arent laundering money");
+
+}
+function quickMath(){
+    var a = prompt("Pick a number");
+    var b = prompt("Pick another number");
+    alert(a + b);
+
+}
+function weSell(){
+    alert("We totally don't sell anything illegal");
+
 }
