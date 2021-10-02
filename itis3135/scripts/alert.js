637cbb92ad5c20d60de28afc2a4927a75e216018
karom12 <kromano@uncc.edu>
2021-10-02 00:04:53 -0400

    1

--- itis3135/scripts/alert.js
+++ itis3135/scripts/alert.js
@@ -6,13 +6,6 @@ function test1()
 {
     alert("Hey my script is running");
 }
-function askName()
-{
-    var name = prompt("What is your name?");
-    var feeling = prompt("How are you doing?");
-    const d1 = new Date();
-    alert("Today is "+ d1 + ". The Totally-Not-Doing-Illegal-Things LLC welcomes you, " + name +"! We're glad you are doing " + feeling +"!");
-}
 function weSell()
 {
     alert("We totally don't sell anything illegal");
