322f04da0169bc634794c310b09062a90afc2141
karom12 <kromano@uncc.edu>
2021-10-02 22:31:18 -0400

    Update polygon-script.js

--- itis3135/scripts/polygon-script.js
+++ itis3135/scripts/polygon-script.js
@@ -2,6 +2,6 @@ var name = prompt("The Ablazed Raccoon LLC would like your age because it is an 
 
 if(age <= 18)
 {
- alert("You are " + age);
-}else{ alert("You are too young!");
+ alert("You are " + age + ". You may enter");
+}else{ prompt("The Ablazed Raccoon LLC would like your age because it is an adult only work site");;
 }
