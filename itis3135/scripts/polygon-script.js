051bc851f361c28c470fb748a3016272a38a6376
karom12 <kromano@uncc.edu>
2021-10-02 22:58:43 -0400

    Update polygon-script.js

--- itis3135/scripts/polygon-script.js
+++ itis3135/scripts/polygon-script.js
@@ -1,4 +1,5 @@
 var age = prompt("Enter your age to enter Ablazed Raccoon LLC site ");
 if( age >= 1){ alert("Welcome to the website");}
-else if (isNAN(age) || age < 1) { age = prompt("Please enter a number");}
+else if (isNAN(age)){ alert("Not a number!");}
+else{prompt("Enter your age to enter Ablazed Raccoon LLC site ");}
 
