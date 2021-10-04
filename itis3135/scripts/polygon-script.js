c5afe94c2ca42421cd51f07cc042dd0dbf5bb75e
karom12 <kromano@uncc.edu>
2021-10-03 21:49:39 -0400

    1

--- itis3135/scripts/polygon-script.js
+++ itis3135/scripts/polygon-script.js
@@ -3,14 +3,14 @@ do{/*It tests to make sure whther or not the while loop will be needed*/
     var age = prompt("Enter your age to enter Ablazed Raccoon LLC site ");
     if(age > 0){/*it returns true if its a proper number for age*/
         alert("Welcome to the website, you are " + age);
-    }else if(age < 0){/*if user inputs a negative, will remake prompt also alert if user inputs a negative*/
+}else if(age < 0){/*if user inputs a negative, will remake prompt also alert if user inputs a negative*/
         alert("Error:Age can't be less than 0");
-    }else if(isNaN(age)){/*same job but for if the user puts in anything other than a number*/
+}else if(isNaN(age)){/*same job but for if the user puts in anything other than a number*/
         alert("Error:Age must be a number");
-    }
+}
 }
 while(age < 0 || isNaN(age));
-/* product of try and fail
+/* product of trial and error
 if(isNaN(age)){
     alert("Error:Age must be a number");
 }else if(age < 0){
@@ -24,7 +24,8 @@ if(isNaN(age)){
 
 function getShape(){
     var sides = prompt("Enter a number between 1 and 10");
-    if (parseInt(sides) == 1){alert("henagon");
+    if (parseInt(sides) == 1){alert("henagon");/*makes sure its rounded up and returns what type of polygon it is
+    same for the rest*/
 }
     else if (parseInt(sides) == 2){alert("digon");
 }
@@ -44,6 +45,7 @@ function getShape(){
 }
     else if (parseInt(sides) == 10){alert("decagon");}
 }
+getShape();
 function validateEntry(){
     var entry = 0;
     if(entry >= 1 && entry <= 10 ){
@@ -53,3 +55,4 @@ function validateEntry(){
     }
     
 }
+validateEntry();
