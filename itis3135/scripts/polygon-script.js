eae280c3da42e8df64e251884bdd22ca8d20b683
karom12 <kromano@uncc.edu>
2021-10-03 20:21:53 -0400

    switch

--- itis3135/scripts/polygon-script.js
+++ itis3135/scripts/polygon-script.js
@@ -1,9 +1,11 @@
 var age = prompt("Enter your age to enter Ablazed Raccoon LLC site ");
-if( age >= 1){ alert("Welcome to the website");
-} /* Welcomes to website if you input a age greater than or equal to 1 */
-else if (isNAN(age)){ prompt("Error:Enter your age to enter Ablazed Raccoon LLC site ");
+if(isNaN(age)){
+    alert("Age must be a number");
+}else if(age < 0){
+    alert("Age can't be less than 0");
+}else{
+    alert("Welcome to the website, you are " + age);
 }
-
 function getShape(){
     var sides = prompt("Enter a number between 1 and 10");
     if (parseInt(sides) == 1){alert("henagon");
