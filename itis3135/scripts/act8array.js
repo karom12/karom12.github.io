4bda7e15bdac6ea06b8d3e8e1a462eb22a57679e
karom12 <kromano@uncc.edu>
2021-10-26 17:56:47 -0400

    1

--- itis3135/scripts/act8array.js
+++ itis3135/scripts/act8array.js
@@ -1,7 +1,7 @@
-const persons = ["Harry Parker", "John Walker"];
-const salaries = ["60,000" , "120,000"];
+const persons = [];
+const salaries = [];
 
-function addSalry(){
+function addSalary(){
     
 }
 function  displayResults(){
