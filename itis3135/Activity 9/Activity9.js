834be5dd01eb140da05fb25ee712c219f787bd03
karom12 <kromano@uncc.edu>
2021-10-26 17:44:59 -0400

    a lot

---
+++ itis3135/Activity 9/Activity9.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+        // get the image URL and id for each image
+
+
+        // preload the image for each link
+
+        // set up the event handlers for each link
+
+
+            // cancel the default action of each link
+
+
+    // move the focus to the second link
+
+}); // end ready
---
+++ itis3135/scripts/act8array.js
@@ -0,0 +1,12 @@
+const persons = ["Harry Parker", "John Walker"];
+const salaries = ["60,000" , "120,000"];
+
+function addSalry(){
+    
+}
+function  displayResults(){
+    
+}
+function addSalrdisplaySalary(){
+    
+}
